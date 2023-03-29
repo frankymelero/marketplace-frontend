5614c16f85aef7ce2a2c55f334e6bd7dbe3d9e1b
unknown <franmelerogallardo@gmail.com>
2023-03-29 19:37:38 +0200

    cosmit

---
+++ src/views/PrivateShopView.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <PrivateMenu />
+    <!-- Esto es una tienda con los productos que vendemos como cursos o web estaticas simples de maquetacion sencillas
+    Las webs que venderiamos como producto serian web basicas y estaticas que pueden ser (me lo invento) 50€, para intentar venderles a los clientes una afiliacion periodica para mantenimiento y actualizacion de la web, vendemos la web barata para atraer y ganar clientes mensuales.
+    los cursos que vendemos (es porque el cesar lo queria con cursos tambien, hay que preguntarle si tiene algun curso montado para subirlo al repo) -->
+    <div class="about">
+        <h2 class="hola">Necesitas una página web?</h2>
+       <p>Tienda</p>
+
+        <img src="../assets/img/taskeasylogo.jpg">
+
+
+    <Footer></Footer>
+</div>
+</template>
+
+<script setup>
+import PrivateMenu from '../components/PrivateMenu.vue';
+import Footer from '../components/Footer.vue';
+</script>
